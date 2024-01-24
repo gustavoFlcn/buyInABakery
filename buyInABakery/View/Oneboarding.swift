121db6749586d83f927fc231a52ae3d0977c87ce
Gustavo Feliciano <gustavo.flcn@gmail.com>
2024-01-23 21:05:28 -0300

    Create the project

---
+++ buyInABakery/View/Oneboarding.swift
@@ -0,0 +1,17 @@
+//
+// Created by Gustavo F. Figueiredo
+// Copyright © Gustavo F. Figueiredo. All Rights Reserved.
+
+
+import SwiftUI
+
+struct Oneboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+#Preview {
+    Oneboarding()
+}
